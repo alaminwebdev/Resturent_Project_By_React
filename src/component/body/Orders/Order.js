425fc5681fb3d41a63d6fbfa2d9553439aa54187
Md Al Amin <alamiin.bd@gmail.com>
2021-12-13 15:10:28 +0600

    some common error fix

--- src/component/body/Orders/Order.js
+++ src/component/body/Orders/Order.js
@@ -1,75 +1,92 @@
-import React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import { Grid, Typography } from '@mui/material';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Skeleton from '@mui/material/Skeleton';
-import Chip from '@mui/material/Chip';
-import dateFormat from 'dateformat';
+import React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import { Grid, Typography } from "@mui/material";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Skeleton from "@mui/material/Skeleton";
+import Chip from "@mui/material/Chip";
+import dateFormat from "dateformat";
 
-
-const Order = props => {
-    console.log(props)
+const Order = (props) => {
+    console.log(props);
     let cartDetailsRow = null;
     if (!props.loading) {
         cartDetailsRow = props.order.cartItems.map((item) => {
             return (
                 <TableRow key={Math.random()}>
-                    <TableCell>{item.dishItem}({item.varient})</TableCell>
+                    <TableCell>
+                        {item.dishItem}({item.varient})
+                    </TableCell>
                     <TableCell align="right">{item.quantity}</TableCell>
                     <TableCell align="right">{item.price}</TableCell>
                 </TableRow>
-            )
-        })
+            );
+        });
     }
 
     return (
         <Grid item lg={3} md={4} sm={6} xs={12}>
-
-            <Card sx={{ mt: 3, }}>
+            <Card sx={{ mt: 3 }}>
                 <CardContent>
-                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                        {props.loading ? (<Skeleton animation="wave" />) : (
-                            <>
-                                Order Id:{props.order.id}
-                            </>
+                    <Typography
+                        sx={{ fontSize: 14 }}
+                        color="text.secondary"
+                        gutterBottom
+                    >
+                        {props.loading ? (
+                            <Skeleton animation="wave" />
+                        ) : (
+                            <>Order Id:{props.order.id}</>
                         )}
                     </Typography>
                     <Typography variant="h6" component="div">
-                        {props.loading ? (<Skeleton animation="wave" />) : (
-                            <>
-                                Address. {props.order.customerInfo.address}
-                            </>
+                        {props.loading ? (
+                            <Skeleton animation="wave" />
+                        ) : (
+                            <>Address. {props.order.customerInfo.address}</>
                         )}
                     </Typography>
-                    <Typography sx={{ mb: 1.5 }} variant="caption" color="text.secondary">
-                        {props.loading ? (<Skeleton animation="wave" />) : (
+                    <Typography
+                        sx={{ mb: 1.5 }}
+                        variant="caption"
+                        color="text.secondary"
+                    >
+                        {props.loading ? (
+                            <Skeleton animation="wave" />
+                        ) : (
                             <>
-                                Order Placed: {dateFormat(props.order.orderTime, "dd mmm , h:MM tt")}
+                                Order Placed:{" "}
+                                {dateFormat(
+                                    props.order.orderTime,
+                                    "dd mmm , h:MM tt"
+                                )}
                                 <Chip
                                     label={props.order.status}
                                     sx={{ ml: 1, fontWeight: 400 }}
                                     size="small"
-
-                                    color={props.order.status === 'delivered' ? 'success' : props.order.status === 'ongoing' ? 'warning' : 'error'}
-
+                                    color={
+                                        props.order.status === "delivered"
+                                            ? "success"
+                                            : props.order.status === "ongoing"
+                                                ? "warning"
+                                                : "error"
+                                    }
                                 />
                             </>
                         )}
                     </Typography>
 
-                    <TableContainer key={Math.random()} >
+                    <TableContainer key={Math.random()}>
                         <Table sx={{}} size="small" aria-label="a dense table">
                             <TableHead>
                                 <TableRow>
-                                    <TableCell >Item</TableCell>
+                                    <TableCell>Item</TableCell>
                                     <TableCell align="right">Qty.</TableCell>
                                     <TableCell align="right">Price</TableCell>
                                 </TableRow>
@@ -77,31 +94,45 @@ const Order = props => {
                             <TableBody>
                                 {props.loading ? (
                                     <TableRow>
-                                        <TableCell><Skeleton animation="wave" /></TableCell>
-                                        <TableCell align="right"><Skeleton animation="wave" /></TableCell>
-                                        <TableCell align="right"><Skeleton animation="wave" /></TableCell>
+                                        <TableCell>
+                                            <Skeleton animation="wave" />
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            <Skeleton animation="wave" />
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            <Skeleton animation="wave" />
+                                        </TableCell>
                                     </TableRow>
-                                ) : (cartDetailsRow)}
+                                ) : (
+                                    cartDetailsRow
+                                )}
                             </TableBody>
                         </Table>
                     </TableContainer>
-
                 </CardContent>
 
                 {props.loading ? (
-                    <CardActions><Skeleton animation="wave" width='100%' /></CardActions>
+                    <CardActions>
+                        <Skeleton animation="wave" width="100%" />
+                    </CardActions>
                 ) : (
-                    <CardActions sx={{ justifyContent: 'space-evenly', mb: 1 }}>
-                        <Chip label={'Price: ' + props.order.totalPrice + 'tk'} variant="outlined" />
-                        <Chip label={'Payment: ' + props.order.customerInfo.payment} variant="outlined" />
+                    <CardActions sx={{ justifyContent: "space-evenly", mb: 1 }}>
+                        <Chip
+                            label={"Price: " + props.order.totalPrice + "tk"}
+                            variant="outlined"
+                        />
+                        <Chip
+                            label={
+                                "Payment: " + props.order.customerInfo.payment
+                            }
+                            variant="outlined"
+                        />
                     </CardActions>
                 )}
-
             </Card>
         </Grid>
+    );
+};
 
-
-    )
-}
-
-export default Order
+export default Order;
--- src/component/body/menu/Dishes/DishItem.js
+++ src/component/body/menu/Dishes/DishItem.js
@@ -1,54 +1,69 @@
-import React, {useState} from 'react';
-import { baseUrl } from '../../../../redux/actionCreators';
-import Varients from './Varients';
-import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import { Grid } from '@mui/material';
-
+import React, { useState } from "react";
+import { baseUrl } from "../../../../redux/actionCreators";
+import Varients from "./Varients";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { Grid } from "@mui/material";
 
 const DishItem = (props) => {
     const [imgExist, setImgExist] = useState(true);
     //console.log(props);
-    const loading = props.loading
-    const dishes = props.dishes
-    const price = props.dishes.price[0]
+    const loading = props.loading;
+    const dishes = props.dishes;
+    const price = props.dishes.price[0];
     //let vari = null;
 
-    const checkImage= imgUrl => {
+    const checkImage = (imgUrl) => {
         var image = new Image();
         image.onload = function () {
             if (this.width > 0) {
                 //console.log("image exists");
-                setImgExist(true)
+                setImgExist(true);
             }
-        }
+        };
         image.onerror = function () {
             //console.log("image doesn't exist");
-            setImgExist(false)
-        }
+            setImgExist(false);
+        };
         image.src = imgUrl;
-    }
-   
+    };
 
     //console.log(price)
     return (
-        <Grid item xs={12} sm={6} md={4} lg={3} >
-            <Card >
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+            <Card>
                 <CardMedia
                     component="img"
                     height="300"
-                    image={imgExist? dishes.image : "http://placehold.jp/300x300.png"}
+                    image={
+                        imgExist
+                            ? dishes.image
+                            : "http://placehold.jp/300x300.png"
+                    }
                     alt="burger"
                     onError={checkImage(dishes.image)}
                 />
 
-                <CardContent sx={{pb:1}}>
-                    <Typography gutterBottom variant="h6" component="div" sx={{ cursor: 'pointer' }} onClick={props.DishSelect}>
-                        {dishes.name.length >20? dishes.name.substring(0,20)+'...' : dishes.name}
+                <CardContent sx={{ pb: 1 }}>
+                    <Typography
+                        gutterBottom
+                        variant="h6"
+                        component="div"
+                        sx={{ cursor: "pointer" }}
+                        onClick={props.DishSelect}
+                    >
+                        {dishes.name.length > 20
+                            ? dishes.name.substring(0, 20) + "..."
+                            : dishes.name}
                     </Typography>
-                    <Varients varients={dishes.varients} price={price} dishID={dishes.id} dishItem={dishes.name} />
+                    <Varients
+                        varients={dishes.varients}
+                        price={price}
+                        dishID={dishes.id}
+                        dishItem={dishes.name}
+                    />
                 </CardContent>
 
                 {/* <CardActions>
@@ -56,8 +71,7 @@ const DishItem = (props) => {
                 </CardActions> */}
             </Card>
         </Grid>
-    )
-}
+    );
+};
 
 export default DishItem;
-
